2e12be21338dc53e1860ccdddc881dc23f0ed089
riyazrahaman <69575788+riyazrahaman@users.noreply.github.com>
2025-12-14 14:26:54 +0530

    Remove Truck component from Header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -19,7 +19,7 @@ export function Header() {
             </div>
           </div>
           <div className="flex items-center gap-1">
-            <Truck size={14} />
+            
             
           </div>
         </div>
